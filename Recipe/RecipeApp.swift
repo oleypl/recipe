8f278721492a6e3997959101cc51b9cb39727f38
Michal Olejniczak <molejniczak88@gmail.com>
2022-03-16 09:13:02 +0100

    Project recipe

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Michal on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
